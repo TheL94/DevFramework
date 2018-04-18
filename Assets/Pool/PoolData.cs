d77f563bdb434fad9f215a19f57e9cf67dd0fd70
FlyingBoar <montecristo310@gmail.com>
2018-04-18 17:19:56 +0200

    - PoolManager

---
+++ Assets/Pool/PoolData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFramework.Pool
+{
+    public class PoolData : ScriptableObject
+    {
+        public string ID;
+        public GameObject Graphic;
+        public int Quantity;
+    }
+}
---
+++ Assets/Pool/PoolManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFramework.Pool
+{
+    public class PoolManager : MonoBehaviour
+    {
+        public List<PoolData> Datas = new List<PoolData>();
+        List<PoolStruct> pools = new List<PoolStruct>();
+
+        public void Init()
+        {
+            if(Datas.Count > 0)
+            {
+                foreach (PoolData data in Datas)
+                {
+                    CreateNewPool(data);
+                }
+            }
+        }
+
+        public GameObject GetObject(string _id)
+        {
+            foreach (PoolStruct pool in pools)
+            {
+                if(_id == pool.Data.ID)
+                {
+                    return pool.ObjectPool.Get();
+                }
+            }
+            return null;
+
+        }
+
+        void CreateNewPool(PoolData _data)
+        {
+            PoolStruct tempStruct = new PoolStruct { Data = GameObject.Instantiate(_data), ObjectPool = new GameObjectPool(_data.Graphic, Instantiate(new GameObject(_data.ID)).transform, _data.Quantity) };
+            pools.Add(tempStruct);
+        }
+    }
+
+    public struct PoolStruct
+    {
+        public PoolData Data;
+        public GameObjectPool ObjectPool;
+    }
+}
