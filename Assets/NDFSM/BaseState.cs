4990d2e688be56e9e638ece077356fae99657efe
Luca <lu.gbelli@hotmail.it>
2018-05-14 23:57:34 +0200

    Inizo lavoro su State Machine

---
+++ Assets/NDFSM/BaseState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFramework.NDFSM
+{
+    public abstract class BaseState
+    {
+        NonDeterministicFSM nDFSM;
+
+        public void OnInit(NonDeterministicFSM _nDFSM)
+        {
+            nDFSM = _nDFSM;
+        }
+
+        public virtual void OnStart() { }
+        public virtual void OnUpdate() { }
+        public virtual void OnEnd() { }
+    }
+}
---
+++ Assets/NDFSM/Transition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFramework.NDFSM
+{
+    public class Transition
+    {
+        public BaseState StartState { get; private set; }
+        public char Symbol { get; private set; }
+        public BaseState EndState { get; private set; }
+
+        public Transition(BaseState _startState, char _symbol, BaseState _endState)
+        {
+            StartState = _startState;
+            Symbol = _symbol;
+            EndState = _endState;
+        }
+    }
+}
+
